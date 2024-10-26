07ce48573b492777b3d6fdaa1b818d760841cded
Rafaelgu27 <127206586+Rafaelgu27@users.noreply.github.com>
2024-10-25 23:47:57 -0300

    Add files via upload

--- Asteroide/index.js
+++ Asteroide/index.js
@@ -1,7 +1,7 @@
 // Adiciona eventos de teclado
 window.addEventListener('keydown', (e) => {
     if(e.keyCode === 27){
-        window.location.href = "../inicio.html";
+        window.location.href = "../index.html";
     }
 });
 
--- Cobrinha/index.js
+++ Cobrinha/index.js
@@ -1,6 +1,6 @@
 // Adiciona eventos de teclado
 window.addEventListener('keydown', (e) => {
     if(e.keyCode === 27){
-        window.location.href = "../inicio.html";
+        window.location.href = "../index.html";
     }
 });
